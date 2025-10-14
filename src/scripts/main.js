7a5d9bb920236021abfd5e85ce75d22826ac4a68
Alex Rodrigues <alexrodriguesw2@hotmail.com>
2025-10-13 22:41:31 -0300

    six update

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,21 @@
-'use strict';
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.querySelector('.contact__forms');
+
+  form.addEventListener('submit', (e) => {
+    // impede o reload da página
+    e.preventDefault();
+
+    // dispara a validação nativa dos campos
+    if (!form.checkValidity()) {
+      // se algum campo inválido, deixa o browser mostrar os erros
+      form.reportValidity();
+
+      return;
+    }
+
+    // aqui todos os campos são válidos
+    form.reset();
+
+    // opcional: feedback visual
+  });
+});
