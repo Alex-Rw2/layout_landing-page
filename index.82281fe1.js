82f96c96f53ef391f4358f548013182cb693c748
Alex Rodrigues <alexrodriguesw2@hotmail.com>
2025-10-19 14:16:43 -0300

    make build

---
+++ index.82281fe1.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",function(){var e=document.querySelector(".contact__forms");e.addEventListener("submit",function(t){if(t.preventDefault(),!e.checkValidity()){e.reportValidity();return}e.reset()})});
+//# sourceMappingURL=index.82281fe1.js.map
